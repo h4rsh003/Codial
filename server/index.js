9ca9a0d64876dba78fe3372483ad59e5c726a132
Harsh Shrivastava <harshshrivastava003@gmail.com>
2025-06-22 14:48:55 +0530

    some changes

--- server/index.js
+++ server/index.js
@@ -11,16 +11,16 @@ connectDB();
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+  origin: "https://codial-woad.vercel.app",
+  credentials: true
+}));
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/uploads", express.static("uploads"));
 app.get("/", (req, res) => res.send("API running"));
 app.use("/api/projects", projectRoutes);
 
-app.use(cors({
-  origin: "https://codial-woad.vercel.app",
-  credentials: true
-}));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
