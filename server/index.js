557f2835aea3109b59a6ad96563139133ecc050a
Harsh Shrivastava <harshshrivastava003@gmail.com>
2025-06-22 14:45:26 +0530

    Deploy on render after addind cors

--- server/index.js
+++ server/index.js
@@ -5,14 +5,12 @@ const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 const projectRoutes = require('./routes/projectRoutes');
-const cors = require('cors');
 
 dotenv.config();
 connectDB();
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/uploads", express.static("uploads"));
