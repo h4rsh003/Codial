d07c6c8d04247f6dc49f913d333fa87a59e80b2f
Harsh Shrivastava <harshshrivastava003@gmail.com>
2025-06-25 00:28:22 +0530

    avatar error removing

--- server/index.js
+++ server/index.js
@@ -19,10 +19,10 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 
+app.use("/uploads", express.static("uploads"));
 if (!fs.existsSync("uploads")) {
   fs.mkdirSync("uploads");
 }
-app.use("/uploads", express.static("uploads"));
 
 app.get("/", (req, res) => res.send("API running"));
 app.use("/api/projects", projectRoutes);
